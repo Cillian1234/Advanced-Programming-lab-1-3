03ddf951a2e5001537538cc84217c051fe7113f0
Cillian <cillsm80@gmail.com>
2024-02-21 20:12:15 +0000

    jart 3 done

--- AP-Lab1/src/partthree/Lab1Part3.java
+++ AP-Lab1/src/partthree/Lab1Part3.java
@@ -2,13 +2,15 @@ package partthree;
 
 import javax.swing.*;
 import java.awt.*;
-
-public class Lab1Part3 extends Frame {
-    Lab1Part3() {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import partone.Lab1Part1;
+public class Lab1Part3 extends Frame implements ActionListener {
         JTextField field = new JTextField();
         JButton button = new JButton("Retrieve student initials from name");
         JLabel label1 = new JLabel("Input student name here", SwingConstants.CENTER);
         JLabel label2 = new JLabel("Result will appear here", SwingConstants.CENTER);
+    Lab1Part3() {
 
         c.gridy = 1;
         c.weighty = 0;
@@ -23,13 +25,17 @@ public class Lab1Part3 extends Frame {
         panel.add(label1, c);
 
         c.gridy = 3;
+        button.addActionListener(this);
         panel.add(button, c);
-
-
 
         setVisible(true);
     }
     public static void main(String[] args) {
         Lab1Part3 run = new Lab1Part3();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        label2.setText(Lab1Part1.getStudentNameFromInitial(field.getText()));
     }
 }
---
+++ AP-Lab1/src/recursive/Lab1Part4.java
@@ -0,0 +1,5 @@
+package recursive;
+
+public class Lab1Part4 {
+    
+}
