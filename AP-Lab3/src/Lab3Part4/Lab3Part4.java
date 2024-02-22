68c7b24b85debca09ff8f20592e14199e7a94552
Cillian <cillsm80@gmail.com>
2024-02-22 23:55:31 +0000

    lab3par4done

--- AP-Lab3/src/Lab3Part4/Lab3Part4.java
+++ AP-Lab3/src/Lab3Part4/Lab3Part4.java
@@ -3,18 +3,76 @@ package Lab3Part4;
 import java.sql.*;
 
 public class Lab3Part4 {
+        static String urlDB = "jdbc:mysql://localhost:3306/ap_lab3";
+        static String usernameDB = "Cillian";
+        static String passwordDB = "Cillian80";
     public static void main(String[] args) {
-        String urlDB = "jdbc:mysql://localhost:3306";
-        String usernameDB = "Cillian";
-        String passwordDB = "Cillian80";
+//        Create("testing", "id INTEGER", "name VARCHAR(45)");
+        read("*", "testing");
+        insert("1", "id", "testing");
+        read("*", "testing");
+        delete("id", "testing", "1");
+        read("*", "testing");
+    }
+
+    public static void Create(String tableName, String firstAttribute, String secondAttribute) {
         try (Connection con = DriverManager.getConnection(urlDB, usernameDB, passwordDB)) {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Statement stmt = con.createStatement();
-            String query = "CREATE TABLE STUDENT" + "(id INTEGER not NULL, " + " first VARCHAR(255), " + " last VARCHAR(255), " + " PRIMARY KEY ( id ))";
+            String query = "CREATE TABLE " + tableName + " (" + firstAttribute + " not null, " + secondAttribute + ")";
+            System.out.println(query);
             stmt.executeUpdate(query);
+        } catch (SQLSyntaxErrorException e) {
+            System.out.println("Error when creating table" + tableName);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
+    public static void read(String selector, String table) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection(urlDB, usernameDB, passwordDB);
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select " + selector + " from " + table);
+            while (rs.next())
+                System.out.println(rs.getInt(1) + " " + rs.getString(2));
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void insert(String data, String column, String table) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con =
+                    DriverManager.getConnection(urlDB, usernameDB, passwordDB);
+                            Statement stmt = con.createStatement();
+            String query = "INSERT INTO " + table + " (" + column + ") VALUES (" + data + ")";
+            stmt.executeUpdate(query);
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void delete(String data, String table, String value) {
+        try (Connection con = DriverManager.getConnection(urlDB, usernameDB, passwordDB)) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Statement stmt = con.createStatement();
+            String query = "DELETE FROM " + table + " " + data + " WHERE " + data + "=" + value;
+            System.out.println("Attempt update " + query);
+            stmt.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
